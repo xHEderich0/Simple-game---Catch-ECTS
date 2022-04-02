be0f983c9d4bb8bee6ab69124bd2369b9a3b8043
xHEderich0 <erni34b@o2.pl>
2022-04-02 16:22:55 +0200

    Enjoy

---
+++ Gierka - Catch ECTS/Gierka - Catch ECTS/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace Gierka__Catch_ECTS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 25.25F);
+            this.textBox1.Location = new System.Drawing.Point(418, 153);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(372, 46);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 40.25F);
+            this.button1.Location = new System.Drawing.Point(696, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(288, 151);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(11, 71);
+            this.trackBar1.Maximum = 7;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(255, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 30.25F);
+            this.button2.Location = new System.Drawing.Point(592, 470);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.button2.Size = new System.Drawing.Size(175, 73);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Comic Sans MS", 20.25F);
+            this.textBox2.Location = new System.Drawing.Point(783, 483);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 46);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(25, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 38);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Difficulty level";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(6, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Easy";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(93, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Medium";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 385);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(275, 161);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(208, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Hard";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.textBox3.Location = new System.Drawing.Point(366, 499);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(134, 44);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 23.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(217, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(172, 44);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Username:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Gierka___Catch_ECTS.Properties.Resources.pp;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(996, 555);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Catch ECTS";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Gierka - Catch ECTS/Gierka - Catch ECTS/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gierka__Catch_ECTS
+{
+    public partial class Form1 : Form
+    {
+        int halfIndex;
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1.Minimum = 1;
+            trackBar1.Maximum = 9;
+            halfIndex = (trackBar1.Maximum + trackBar1.Minimum) / 2;  //zeby ustawiac poczatkowa wartosc trackBar1 na srodku i do poprawnego dzialania poziomu trudnosci
+            trackBar1.Value = halfIndex;                              //  
+            nowy.Click += new System.EventHandler(this.myScore); //ta linia tutaj zeby punkty mogly sie resetowac, a nie dodawac w nieskonczonosc podczas ich zdobycia
+            //timer1.Interval = 1500;
+            //textBox3.Text = "1500"; // tego textBoxa nie bedzie, tylko do sprawdzania
+        }
+        Button nowy = new Button();
+        Random rnd = new Random();
+        Image CW10 = Image.FromFile("CW10.jpg");
+        Image PP = Image.FromFile("pp.jpg");
+        //Image B = Image.FromFile("B.png");
+        Point loc = new Point();
+        Size size = new Size(115, 90);
+        int counter = 0; //calosc
+        int score = 0; //zdobyte
+        int t=1500; //wartosc medium
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // dodac timer do rozpoczecia gry
+            // im wiecej punktow tym trudniej
+            //counter = 0;
+            //score = 0;
+            nowy.Visible = true;
+
+            textBox1.Visible = false;
+            button1.Visible = false;
+            label5.Visible = false;
+
+            button2.Visible = true;
+            textBox2.Visible = true;
+            
+            this.BackgroundImage = CW10;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            //nowy.BackgroundImage = B;
+            //nowy.BackgroundImageLayout = ImageLayout.Stretch;
+            nowy.Text = "ECTS";
+            nowy.Font = new Font("Comic Sans MS", 24);
+            nowy.BackColor = Color.PeachPuff;
+ 
+            groupBox1.BackColor = Color.SandyBrown;
+            label1.ForeColor = Color.Navy;
+            label2.ForeColor = Color.Navy;
+            label3.ForeColor = Color.Navy;
+            label4.ForeColor = Color.Navy;
+
+            nowy.Size = size;
+            loc = nowy.Location;
+            loc.X = rnd.Next(900);
+            loc.Y = rnd.Next(300);
+            nowy.Location = loc;
+            Controls.Add(nowy);
+
+            //timer1.Enabled = true;
+            //timer1.Interval = 1; //zeby nie czekac
+            //timer1.Enabled = false;
+            timer1.Enabled = true;
+            textBox2.Text = score.ToString() + "/" + counter.ToString() + " ECTS";
+            //button1.Click += new System.EventHandler(this.timer1_Tick);
+        }
+
+        private void changePosition()
+        {
+            //timer1.Enabled = true;
+            if (nowy.Visible == true)   //zeby nie tylko zmienialo pozycje bez delaya, ale zeby button znikal(z delayem) i pojawial sie
+            {
+                nowy.Visible = false;
+                timer1.Enabled = false;
+            }
+            else
+            {
+                nowy.Visible = true;
+                timer1.Enabled = false;
+            }
+            //textBox3.Text = Convert.ToString(timer1.Interval); // jaki interwał
+            timer1.Enabled = true;
+            //Point loc = nowy.Location;
+            loc.X = rnd.Next(900);
+            loc.Y = rnd.Next(300);
+            nowy.Location = loc;
+            counter++;
+
+            if(nowy.Visible == true)
+                textBox2.Text = score.ToString() + "/" + (counter--).ToString() + " ECTS"; //zeby nie naliczalo podwojnie countera jak bedzie znikac(zeby punkty sie zgadzaly)
+            else
+                textBox2.Text = score.ToString() + "/" + counter.ToString() + " ECTS";     //zwykly counter
+
+            Controls.Add(nowy);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {            
+            changePosition();
+
+            timer1.Interval = t; //do utrzymania poprawnosci dzialania timera w dalszych krokach(po zmianie poziomu trudnosci)
+            timer1.Interval += rnd.Next((int)(-timer1.Interval * 0.2), (int)(timer1.Interval * 0.2)); //mala losowosc momentu pojawiania sie buttona i jego znikania
+
+            //textBox3.Text = Convert.ToString(timer1.Interval); do eksperymentow
+        }
+
+            private void myScore(object sender, EventArgs e)
+        {
+            //timer1.Enabled = false; //nie musi to byc
+            //timer1.Enabled = true;  //
+
+            changePosition();
+            //counter++;  
+            score++;
+
+            textBox2.Text = score.ToString() + "/" + counter.ToString() + " ECTS"; 
+            //Controls.Add(nowy);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            groupBox1.BackColor = Color.MidnightBlue;
+            label1.ForeColor = Color.Red;
+            label2.ForeColor = Color.Red;
+            label3.ForeColor = Color.Red;
+            label4.ForeColor = Color.Red;
+
+            using (StreamWriter saveScore = new StreamWriter("Score.txt", true)) //jesli na nowym obiekcie wykonamy wszystkie akcje w klamrach, plik sie zamyka i zapisuje, wywalając go z pamieći RAM
+            {
+                String sDate = DateTime.Now.ToString();
+                saveScore.WriteLine(textBox1.Text + "   <|>   " + textBox2.Text + "   <|>   " + sDate);
+            }
+            textBox1.Visible = true;
+            button1.Visible = true;
+            label5.Visible = true;
+
+            button2.Visible = false;
+            textBox2.Visible = false;
+
+            timer1.Enabled = false; //zatrzymanie timera zeby button nie latał po skoczeniu gry
+
+            nowy.Visible = false;
+            this.BackgroundImage = PP;
+
+            textBox1.Text = ""; //reset licznika punktacji i wymazanie textboxów
+            score = 0;          //
+            counter = 0;        //
+            textBox2.Text = ""; //
+
+            trackBar1.Value = (trackBar1.Maximum + trackBar1.Minimum) / 2; //4 linijki w STOP dla poprawnego dzialania poziomu trudnosci przy kolejnych grach
+            halfIndex = (trackBar1.Maximum + trackBar1.Minimum) / 2;       //
+            timer1.Interval = 1500;                                        //
+            t = 1500;                                                      //
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e) //wlasciwe działanie poziomu trudnosci
+        {
+            int ratioChanger = 250;
+
+            if (trackBar1.Value < halfIndex)
+            {
+                t += (Math.Abs(halfIndex - trackBar1.Value)) * ratioChanger;
+                halfIndex = trackBar1.Value;
+                timer1.Interval = t;
+            }
+            else if (trackBar1.Value > halfIndex)
+            {
+                t -= (Math.Abs(halfIndex - trackBar1.Value)) * ratioChanger;
+                halfIndex = trackBar1.Value;
+                timer1.Interval = t; 
+            }
+
+            //textBox3.Text = Convert.ToString(timer1.Interval); //do eksperymentow
+        }
+    }
+}
